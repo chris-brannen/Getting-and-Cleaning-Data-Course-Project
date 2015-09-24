dcdba78496045011d89b4594295181b620fa14f5
chris-brannen <christopher.m.brannen@gmail.com>
2015-09-23 20:21:02 -0400

    changed sort order of final tidy data set

--- run_analysis.R
+++ run_analysis.R
@@ -65,7 +65,7 @@ tidy_data <- aggregate(prep_for_tidy_data[,3:88], by = list(prep_for_tidy_data$S
 colnames(tidy_data) <- colnames(prep_for_tidy_data)
 
 # Sort tidy data set first by Subject, then by Activity
-tidy_data <- tidy_data[order(tidy_data$Subject, tidy_data$Activity),]
+tidy_data <- tidy_data[order(tidy_data$Subject, tidy_data$Activity),]  
 
 # Write the tidy data set out to a text file named tidy_data.txt without row names.
 write.table(tidy_data, file="data\\tidy_data.txt",row.names=FALSE,col.names=TRUE)
